4168f37d8d76c69132732f0f790290cfc61c2e8b
DemonZito <madeleine.day@hotmail.com>
2017-06-02 16:27:36 +1200

    CURSOR

--- Solitaire/FrameWork/game.cpp
+++ Solitaire/FrameWork/game.cpp
@@ -62,7 +62,7 @@ CGame::Initialise(HINSTANCE _hInstance, HWND _hWnd, int _iWidth, int _iHeight)
     m_pBackBuffer = new CBackBuffer();
     VALIDATE(m_pBackBuffer->Initialise(_hWnd, _iWidth, _iHeight));
 
-	ShowCursor(false);
+	ShowCursor(true);
 
     return (true);
 }
